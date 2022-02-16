1456839b5ab080021ba22ccd2f6771ada08c2066
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-16 14:43:34 +0600

    Added change to scr.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// document.querySelector('input[type="text]').addEventListener('focus', function(){
+//   this.value = "Hello";
+
+// });
+// document.querySelector('input[type="text]').addEventListener('blur', function(){
+  // this.value = "Bye";
+
+// });
+// document.querySelector('input[type="text"]').addEventListener('focus', function(){
+//     this.style.borderColor = "initial";
+  
+//   });
+//   document.querySelector('input[type="text"]').addEventListener('blur', function(){
+//     if (this.value == false) {
+//       this.style.borderColor = " red";
+//     }
+   
+  
+//   });
+
+
+document.querySelector('input[type="text"]').addEventListener('change', function(){
+  alert('changed')
+});
+
